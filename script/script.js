167b3a37fda8e80b0f3ce8f8fce79ec75f0a7500
DESKTOP-BVNGPMK\TianTai <admin@localhost>
2023-03-11 12:44:12 +0800

    更新

---
+++ script/script.js
@@ -0,0 +1,41 @@
+$(function () {
+	const context = '/3sYPGmBhlnM+WHURuyyzaj4BhqPdcAOlUOj5V+MIIs=';
+	$('.password>input').on('keydown', function (e) {
+		if (e.keyCode === 13) {
+			var password = $(this).val();
+			var str = CryptoJS.AES.decrypt(context, CryptoJS.enc.Utf8.parse(md5(password, 16)), {
+	            iv: CryptoJS.enc.Utf8.parse(md5(password, 16)),
+	            mode: CryptoJS.mode.CBC,
+	            padding: CryptoJS.pad.Pkcs7
+	        }).toString(CryptoJS.enc.Utf8);
+	        if (str) {
+	        	$('.avatar').addClass('door-rotate');
+				$('.door-left').animate({'left': '-50%'}, 1500);
+				$('.door-right').animate({'right': '-50%'}, 1500);
+				$('.avatar').animate({'margin-left': '-180px'}, 1500);
+				$('.password').animate({'margin-left': '-190px'}, 1500);
+	        	var timer = setTimeout(function () {
+	        		window.location.href = str;
+	        	}, 1500);
+	        }else {
+			    for (i = 1; i < 7; i++) {
+			        $('.password').animate({
+			            'left': '-=5'
+			        }, 3, function() {
+			            $(this).animate({
+			                'left': '+=10'
+			            }, 3, function() {
+			                $(this).animate({
+			                    'left': '-=5'
+			                }, 3, function() {
+			                    $(this).animate({
+			                        'left': '100%'
+			                    }, 3, function() {});
+			                });
+			            });
+			        });
+			    }
+	        }
+		}
+	});
+});
